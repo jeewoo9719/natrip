da736fd69ed9e1d8f505edb01b9f6c0076bab6ff
kanghyejin0106 <34128604+kanghyejin0106@users.noreply.github.com>
2017-12-02 16:12:47 +0900

    ds
    dfdx

--- app/src/main/java/com/example/chanyoung/nattrip/Register.java
+++ app/src/main/java/com/example/chanyoung/nattrip/Register.java
@@ -8,8 +8,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Iterator;
 
 public class Register extends AppCompatActivity {
     DatabaseReference table; //데이터베이스 레퍼런스 객체 선언
@@ -33,45 +38,70 @@ public class Register extends AppCompatActivity {
         ed_name = (EditText) findViewById(R.id.ed_name);
         ed_email = (EditText) findViewById(R.id.ed_email);
 
+        table = FirebaseDatabase.getInstance().getReference("users");
+
         Button id_check = (Button) findViewById(R.id.btn_id_check);
         id_check.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 check_id=true; //중복확인
-                if(check_id==false){
-                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(Register.this);
-                    alertDialog.setTitle("아이디 중복 확인").setMessage("이미 등록된 아이디입니다.");
-                    alertDialog.setPositiveButton("확인", null);
-                    alertDialog.show();
-                }else if(check_id==true){
-                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(Register.this);
-                    alertDialog.setTitle("아이디 중복 확인").setMessage("확인되었습니다.");
-                    alertDialog.setPositiveButton("확인", null);
-                    alertDialog.show();
-                }
+                table.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Iterator i  = dataSnapshot.getChildren().iterator();
+                        while(i.hasNext()){
+                            if(ed_id.getText().toString().equals(((DataSnapshot)i.next()).getKey().toString())) {
+                                check_id = false;
+                                //Toast.makeText(getApplicationContext(), "dfdsa", Toast.LENGTH_SHORT).show();
+                                break;
+                            }
+                        }
+                        if(check_id==false){
+                            AlertDialog.Builder alertDialog = new AlertDialog.Builder(Register.this);
+                            alertDialog.setTitle("아이디 중복 확인").setMessage("이미 등록된 아이디입니다.");
+                            alertDialog.setPositiveButton("확인", null);
+                            alertDialog.show();
+                        }else if(check_id==true){
+                            AlertDialog.Builder alertDialog = new AlertDialog.Builder(Register.this);
+                            alertDialog.setTitle("아이디 중복 확인").setMessage("확인되었습니다.");
+                            alertDialog.setPositiveButton("확인", null);
+                            alertDialog.show();
 
-            }
-        });
+                        }
 
-        final Button regist_button = (Button) findViewById(R.id.btn_regi);
-        regist_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                        final Button regist_button = (Button) findViewById(R.id.btn_regi);
+                        regist_button.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
 
-                if(ed_pw.getText().toString().equals(ed_re_pw.getText().toString())//비밀번호 확인
-                        && check_id==true  ){
-                    regiUser();
+                                if(ed_pw.getText().toString().equals(ed_re_pw.getText().toString())//비밀번호 확인
+                                        && check_id==true  ){
+                                    regiUser();
                     /*Intent sign_in_page = new Intent(Register.this,SignIn.class);
                     startActivity(sign_in_page);*/
-                    finish();
-                }else if(check_id!=true){
-                    Toast.makeText(getApplicationContext(),"아이디를 다시 확인해주세요",Toast.LENGTH_LONG).show();
-                }else if(ed_pw.getText().toString()!=ed_re_pw.getText().toString()){
-                    Toast.makeText(getApplicationContext(),"비밀번호를 다시 확인해주세요",Toast.LENGTH_LONG).show();
-                }
+                                    finish();
+                                }else if(check_id!=true){
+                                    Toast.makeText(getApplicationContext(),"아이디를 다시 확인해주세요",Toast.LENGTH_LONG).show();
+                                }else if(ed_pw.getText().toString()!=ed_re_pw.getText().toString()){
+                                    Toast.makeText(getApplicationContext(),"비밀번호를 다시 확인해주세요",Toast.LENGTH_LONG).show();
+                                }
+
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
 
             }
         });
+
+
     }
     public void regiUser() {//데이터베스 연결
         table = FirebaseDatabase.getInstance().getReference("users");
